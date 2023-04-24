e26f1336256d66f3fe64c0f411f40b3c59c26e46
Samuel Borges <sammyzord.dev@gmail.com>
2023-04-24 12:48:00 +0200

    :tada: initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TitleSection from "./components/TitleSection.vue";
+import AboutSection from "./components/AboutSection.vue";
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center image">
+    <TitleSection></TitleSection>
+    <AboutSection></AboutSection>
+  </div>
+</template>
+
+<style scoped>
+.image {
+  background-image: url("./assets/stock-photo.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+</style>
---
+++ src/components/AboutSection.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="flex flex-col items-center bg-stone-200 w-10/12">aaaa</div>
+</template>
